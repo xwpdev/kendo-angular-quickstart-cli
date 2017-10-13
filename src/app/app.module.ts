dcb8dca1875e4bbf48f6bd190087a37e4f605596
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2017-10-13 10:40:36 +0300

    chore: add animations module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -40,6 +41,7 @@ load(
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
 
